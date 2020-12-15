986e16a5d867d338d9ee99e9297203218d39d364
source-architect <source.architect@gmail.com>
2020-12-15 20:14:39 +0330

    finished 404 page, working on block page design and posts data structure

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,29 @@
 import React from "react"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
+
+import Logo from "../images/logo.svg"
 
 const NotFoundPage = () => (
-  <div className="fof-page">
-    <SEO title="404: Not found" />
-    <h1 style={{ color: "#fff" }}>test</h1>
+  <div className="fof-page d-flex align-items-center px-4 justify-content-center text-light font-weight-light">
+    <SEO title="404 |Not found" />
+    <div className="text-area row align-items-center justify-content-center">
+      <div className="container-404 col-12 col-md-4 text-center">404</div>
+      <div className="col-12 col-md-6 text-center text-md-left">
+        <div className="container-text font-italic text-capitalize mb-4">
+          page was not found
+        </div>
+        <div className="container-link d-flex flex-column flex-lg-row align-items-center justify-content-center justify-content-lg-start">
+          <Link to="/" className="link text-capitalize mr-3">
+            go to home page |
+          </Link>
+          <div className="brand d-inline-block text-uppercase font-weight-normal">
+            <img src={Logo} alt="travel-go-logo" className="image mr-2" />
+            travel go
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 )
 
